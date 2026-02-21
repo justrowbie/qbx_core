8be04402173cbb2f4fc83e0e214fb89c09cf5efb
rowbie <rowbiefirstson@gmail.com>
2026-02-22 00:49:55 +0300

    Update starterkit

--- config/shared.lua
+++ config/shared.lua
@@ -4,5 +4,14 @@ return {
     notifyPosition = 'top-right', -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
     ---@type { name: string, amount: integer, metadata: fun(source: number): table }[]
     starterItems = { -- Character starting items
+        { name = 'water_bottle', amount = 10 },
+        { name = 'snikkel_candy', amount = 10 },
+        { name = 'bandage', amount = 5 },
+        { name = 'radio', amount = 1 },
+        { name = 'tent', amount = 1 },
+        { name = 'campfire', amount = 1 },
+        { name = 'map', amount = 1 },
+        { name = 'weapon_pistol', amount = 1 },
+        { name = 'ammo-9', amount = 50 },
     }
 }
